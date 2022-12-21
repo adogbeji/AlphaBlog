732c00a7a54c738ebd9288275b2204537d76a56c
Ubuntu <ubuntu@ip-172-31-10-29.eu-west-2.compute.internal>
2022-12-21 11:44:40 +0000

    Add code to save article & redirect to article page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,6 +13,8 @@ class ArticlesController < ApplicationController
     def create
         # render plain: params[:article]
         @article = Article.new(params.require(:article).permit(:title, :description))
-        render plain: @article.inspect
+        @article.save
+        # redirect_to article_path(@article)  # Origina redirect code
+        redirect_to @article  # Redirect shortcut
     end
 end
